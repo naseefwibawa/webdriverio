04de559c04ed3143d68a6b49b394d79d4f0545e8
Naseef <naseefwibawa@gmail.com>
2022-11-01 16:13:15 +0700

    commit to master branch

---
+++ test/specs/android/add-checklist.spec.js
@@ -0,0 +1,32 @@
+const {
+	default: addChecklist,
+} = require('../../screenobject/android/add-checklist')
+const { default: addNote } = require('../../screenobject/android/add-note')
+const { default: editNote } = require('../../screenobject/android/edit-note')
+
+describe('checklist', () => {
+	before(async () => {
+		await editNote.skipTutorial()
+	})
+	it('add checklist and verify', async () => {
+		await addNote.addNoteTxt.click()
+		await addChecklist.addChecklistBtn.click()
+		await expect(addNote.editNoteTxt).toBeDisplayed()
+
+		await addNote.noteTitleField.setValue('haha')
+		await addChecklist.addItem.click()
+		await addChecklist.checklistTxtField.setValue('test123')
+		await addChecklist.checklistNextBtn.click()
+		await addChecklist.checklistTxtField.setValue('automation is fun')
+		await addChecklist.checklistOkBtn.click()
+		const checklistText = await addChecklist.checklistTxt[1].getText()
+		await expect(checklistText).toEqual('automation is fun')
+
+		await $(
+			'//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/color_btn"]'
+		).click()
+		await $(
+			'//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/txt7"]'
+		).click()
+	})
+})
